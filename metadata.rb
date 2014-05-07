ae18b6f5a37d14bf6cd02ca6722bcff15e9f3b62
Victor Lin <bornstub@gmail.com>
2014-05-07 10:34:22 -0700

    Update cookbook

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
 depends          'balanced-apt'
+depends          'balanced-postgresql'
 depends          'python'
-depends          'postgresql'
+depends          'git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,45 @@
 include_recipe 'balanced-apt'
+include_recipe 'balanced-postgresql'
+include_recipe 'balanced-postgresql::server'
+include_recipe 'balanced-postgresql::client'
+
+include_recipe 'python'
+include_recipe 'git'
+
+directory '/srv' do
+  action :create
+end
+
+git '/srv/benchmark' do
+  repository 'https://github.com/balanced/aggregation-benchmark.git'
+  action :sync
+end
+
+python_virtualenv '/srv/benchmark/.env' do
+  action :create
+end
+
+python_pip 'psycopg2' do
+  virtualenv '/srv/benchmark/.env'
+end
+
+bash 'install-benchmark' do
+  code <<-EOF
+  	cd /srv/benchmark
+  	.env/bin/pip install .
+  EOF
+end
+
+pg_user 'benchmark' do
+  privileges superuser: true, createdb: true, login: true
+  password 'benchmark'
+end
+
+pg_database 'benchmark' do
+  owner 'benchmark'
+end
+
+template '/opt/benchmark.sh' do
+  source 'benchmark.sh.erb'
+  mode '755'
+end
